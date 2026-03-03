9dba37d645e46b32fa7d0df19f42b7ace4588927
Tomas-Pauwels <tomas.pauwels@live.be>
2026-03-03 09:25:49 +0100

    add a function to play one round of rps
    The function playRound is a very stretched out
    nested mess to play one round of rps.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log("computer chose :" + computerSelection);
+playRound(humanSelection,computerSelection);
+console.log("the human score is " + humanScore + "the computer score is " + computerScore);
 
 function  getComputerChoice() {
     selector = Math.random()*3;
@@ -15,7 +23,44 @@ function  getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let human_choice = prompt("Choose either 'rock', 'paper' or scissors.");
-    console.log(human_choice);
+    // let human_choice = prompt("Choose either 'rock', 'paper' or scissors.");
+    let human_choice = "scissors"
+    // console.log(human_choice);
     return human_choice;
+}
+
+function playRound(humanChoice,computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "scissors"){
+        if (computerChoice === "rock") {
+            console.log("You lose, rock beats scissors");
+            computerScore = ++computerScore;
+        } else if (computerChoice === "scissors") {
+            console.log("It's a draw, you both chose scissors");
+        } else {
+            console.log("You win! scissors beat paper");
+            humanScore = ++humanScore;
+        }
+    } else if (humanChoice === "rock"){
+        if (computerChoice === "paper"){
+            console.log("You lose, paper beats rock");
+            computerScore = ++computerScore;
+        } else if (computerScore === "rock") {
+            console.log("It's a draw, you both chose rock");
+        } else {
+            console.log("You win! rock beats scissors");
+            humanScore = ++humanScore;
+        }
+    } else { //paper
+        if (computerChoice === "scissors") {
+             console.log("You lose, scissors beats paper");
+            computerScore = ++computerScore;
+        } else if (computerChoice === "paper") {
+            console.log("It's a draw, you both chose paper");
+        } else {
+            console.log("You win! paper beats rock");
+            humanScore = ++humanScore;
+        }
+    }
 }
